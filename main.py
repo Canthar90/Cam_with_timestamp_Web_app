7342ed12c01462a0297ac918e36fc4efa0088a24
Canthar90 <d.cieslak90@gmail.com>
2023-01-12 13:33:08 +0100

    Added text's displaying week day and time

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ st.title("Monitor Detector")
 
 start = st.button("Start Camera")
 
+
 if start:
     streamlit_image = st.image([])
     camera = cv2.VideoCapture(0)
-    
+
     while True:
+        today = dt.datetime.now()
+        day_name = today.strftime("%A")
+        clock = today.strftime("%H:%M:%S")
         check, frame = camera.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         
-        cv2.putText(img=frame, text="Hello", org=(50, 50),
+        cv2.putText(img=frame, text=day_name, org=(50, 50),
                     fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
                     color=(20, 100, 200), thickness=2, lineType=cv2.LINE_AA)
+        
+        cv2.putText(img=frame, text=clock, org=(500, 450),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
+                    color=(255, 0, 0), thickness=2, lineType=cv2.LINE_AA)
         
         streamlit_image.image(frame)
